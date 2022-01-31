aa333d0fc995c9896c6614e6dba307e7610c0060
liwj <773577089@qq.com>
2022-01-31 15:45:41 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'vue/setup-compiler-macros': true,
+
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:vue/vue3-recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': [0, { 'packageDir ': './src/' }],
+  },
+};
---
+++ stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-config-recommended-vue',
+  ],
+  rules: {
+  },
+};
