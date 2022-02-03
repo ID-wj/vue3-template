af94c1512619850a23afe1ab9b237c5046f1954c
liwj <773577089@qq.com>
2022-02-03 11:50:53 +0800

    add: 增加pinia

--- src/views/page-1.vue
+++ src/views/page-1.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>page</div>
+  <div>page{{ name }}</div>
 </template>
 
 <script lang="ts" setup>
+import { useStore } from '../store';
 
+const store = useStore();
+const name = store.getName;
 </script>
